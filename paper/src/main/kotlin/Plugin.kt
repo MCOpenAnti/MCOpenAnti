a53d98d065789eff6c1ddcd5db7ec11158c2bb87
eveeifyeve <eveeg1971@gmail.com>
2024-06-30 00:09:09 +1000

    feat: flag system

--- paper/src/main/kotlin/Plugin.kt
+++ paper/src/main/kotlin/Plugin.kt
@@ -2,6 +2,7 @@ package net.teaclient.anti
 
 import org.bukkit.plugin.java.JavaPlugin
 
+public var flags = mapOf<String, Int>()
 class Plugin : JavaPlugin() {
     override fun onEnable() {
         logger.info("AntiCheat has been enabled!")
