614877fa492e035434f4b5080d31c9895afc80a2
eveeifyeve <eveeg1971@gmail.com>
2024-06-30 00:09:18 +1000

    feat: fly check init

--- paper/src/main/kotlin/Listener.kt
+++ paper/src/main/kotlin/Listener.kt
@@ -1,10 +1,12 @@
 package net.teaclient.anti
 
 import net.kyori.adventure.text.Component
-import org.bukkit.Bukkit.getOperators
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
+import org.bukkit.event.player.PlayerMoveEvent
+import org.bukkit.Bukkit.*
+import org.bukkit.util.Vector
 
 class Listener : Listener {
 
@@ -15,4 +17,25 @@ class Listener : Listener {
             operator.player?.sendMessage(Component.text("[[TeaClientANTI]]: Player: " + event.player.name + " Is running on: " + event.player.clientBrandName))
         }
     }
+
+    @EventHandler
+    fun onPlayerMove(event: PlayerMoveEvent) {
+        val player = event.player;
+        var savedPositions = mapOf<String, Array<Vector>>()
+        if (player.isValid || player.isInWater) {
+            savedPositions = savedPositions.plus(player.name to (savedPositions[player.name] ?: arrayOf(player.location.toVector())))
+            val lastPositions = savedPositions[player.name]
+            if (lastPositions == null || lastPositions.size < 4) return
+
+            player.sendMessage(Component.text("You are " + (player.location.y - lastPositions[lastPositions.size - 3].y) + " blocks away from your previous position."))
+//            val position = lastPositions[lastPositions.size - 3]
+//
+//            val yDistance = (player.location.y - position.y)
+//            player.sendMessage(Component.text("You are $yDistance blocks away from your previous position."))
+
+
+        }
+
+    }
 }
+
